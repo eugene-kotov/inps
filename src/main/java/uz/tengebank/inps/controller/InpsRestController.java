b9955cc33a476ebe51cb507928df2472947cf955
Eugene Kotov <eugene_kotov@hotmail.com>
2020-04-02 10:20:51 +0500

    add exception handler

--- src/main/java/uz/tengebank/inps/controller/InpsRestController.java
+++ src/main/java/uz/tengebank/inps/controller/InpsRestController.java
@@ -1,24 +1,21 @@
 package uz.tengebank.inps.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import uz.tengebank.inps.model.dto.InpsRq;
+import uz.tengebank.inps.model.dto.*;
 import uz.tengebank.inps.persistence.InpsRepository;
 import uz.tengebank.inps.service.InpsService;
 
 @RestController
+@AllArgsConstructor
 public class InpsRestController {
 
     private final InpsService inpsService;
     private final InpsRepository inpsRepository;
-
-    public InpsRestController(InpsService inpsService, InpsRepository inpsRepository) {
-        this.inpsService = inpsService;
-        this.inpsRepository = inpsRepository;
-    }
 
     @PostMapping(path = "/inps", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> sendInps(@RequestBody InpsRq rq) {
